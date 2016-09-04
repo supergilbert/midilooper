9fc24b6a2a0b61e6dc243206210ff84349407396
Gilbert <gilbr@phenom>
2016-09-04 14:34:52 +0200

    packaging encore

--- src/gen_debchangelog.sh
+++ src/gen_debchangelog.sh
@@ -5,7 +5,10 @@ CURRENT_DIR=$(dirname $0)
 cp ${CURRENT_DIR}/debian/changelog.in ${CURRENT_DIR}/debian/changelog
 
 cd ${CURRENT_DIR}
-LAST_VERSION=$(dpkg-parsechangelog --show-field Version)
-TIMESTAMP=$(git rev-list --timestamp --max-count=1 HEAD | cut -d' ' -f1)
-debchange --newversion "${LAST_VERSION}.${TIMESTAMP}" "Temporary dev modification"
+VERSION=$(dpkg-parsechangelog -S Version)
+if ! (git log --max-count=1 --pretty="%d" | grep tag | grep -q $VERSION); then
+    LAST_VERSION=$(dpkg-parsechangelog --show-field Version)
+    TIMESTAMP=$(git rev-list --timestamp --max-count=1 HEAD | cut -d' ' -f1)
+    debchange --newversion "${LAST_VERSION}.${TIMESTAMP}" "Temporary dev modification"
+fi
 cd -
